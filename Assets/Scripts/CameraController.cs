30c62d534c128325f9b6e283061285f94264d859
Liu <Nagisa3113@gmail.com>
2019-06-18 20:00:06 +0800

    加入摄像机跟随

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+
+
+    Transform target;
+    Vector3 offset;
+    Vector3 cameraVelocity = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        target = GameObject.Find("Player").transform;
+        offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, target.position + offset, ref cameraVelocity, 0.1f);
+    }
+
+
 }
